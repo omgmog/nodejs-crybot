50fb81fccd1285347a1d231783adb632f2dffa78
Max Glenister <moglenstar@gmail.com>
2014-06-12 22:59:34 +0100

    Updated example code to use a light sensor (as that's what I have at hand)

--- crybot.js
+++ crybot.js
@@ -1,12 +1,23 @@
 var five = require("johnny-five"),
     board = new five.Board();
 
-board.on("ready", function() {
-    var sonar = new five.Sonar("A0");
+board.on("ready", function () {
+    var photoresistor = new five.Sensor({
+        pin: "A0",
+        freq: 250
+    });
+    var led = new five.Led(11);
 
-    sonar.on("data", function() {
-        if (this.cm > 5) {
-            // TODO: cry all the things
+    board.repl.inject({
+        pot: photoresistor
+    });
+
+    photoresistor.on("data", function () {
+        console.log('Brightness:', this.value);
+        if (this.value > 15) {
+            led.on();
+        } else {
+            led.off();
         }
     });
 });
